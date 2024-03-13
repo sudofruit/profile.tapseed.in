acf788b4d9761a6a9a5628a5aa8fb7417b53e503
adithyanp95 <padithyan@gmail.com>
2024-03-13 21:57:08 +0530

    id atribute not find fixed

--- add-link/scripts.js
+++ add-link/scripts.js
@@ -33,15 +33,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const idName = event.target.id;
 
         // Access the value of data-icon attribute
-        var dataIconValue = document
-          .querySelector(`[data-icon="${idName}"]`)
-          .getAttribute("data-icon");
-
-        // Check if the data-icon element exists
-        if (!dataIconValue) {
+        var dataIconElement = document.querySelector(`[data-icon="${idName}"]`);
+        if (!dataIconElement) {
           console.error("Error: data-icon attribute not found.");
           return;
         }
+
+        var dataIconValue = dataIconElement.getAttribute("data-icon");
 
         // Get the text content of the <h5> element and convert it to lowercase
         h5Text = document
@@ -73,57 +71,55 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("No ID");
       }
     });
-});
 
-document.getElementById("addButton").addEventListener("click", function () {
-  // Retrieve data from the DOM
-  var username = "abhin";
-  var linkTextValue = document.getElementById("linkText").value;
-  var linkValue = document.getElementById("link_value").value;
+  document.getElementById("addButton").addEventListener("click", function () {
+    // Retrieve data from the DOM
+    var username = localStorage.getItem("username");
+    var token = localStorage.getItem("token");
 
-  var link_on = true;
-  var link_name = h5Text; // Use h5Text as link_name
+    var linkTextValue = document.getElementById("linkText").value;
+    var linkValue = document.getElementById("link_value").value;
 
-  console.log("username:" + username);
-  console.log("link_text : " + linkTextValue);
-  console.log("link : " + linkValue);
-  console.log("link_on : " + link_on);
-  console.log("link_name : " + link_name);
+    var link_on = true;
+    var link_name = h5Text; // Use h5Text as link_name
 
-  // Construct the data object
-  var postData = {
-    data: {
-      username: username,
-      link: linkValue,
-      link_text: linkTextValue,
-      link_on: link_on,
-      link_name: link_name,
-    },
-  };
+    console.log("username:" + username);
+    console.log("link_text : " + linkTextValue);
+    console.log("link : " + linkValue);
+    console.log("link_on : " + link_on);
+    console.log("link_name : " + link_name);
 
-  console.log(postData);
+    // Construct the data object
+    var postData = {
+      data: {
+        username: username,
+        link: linkValue,
+        link_text: linkTextValue,
+        link_on: link_on,
+        link_name: link_name,
+      },
+    };
 
-  // Define your token
-  var token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMDI3NDcyMCwiZXhwIjoxNzEyODY2NzIwfQ.1Fk-fjcu7U0L0JekSWl442pzhkKsEdq8KmsRUx9zyqw";
+    console.log(postData);
 
-  // Construct the headers with the token
-  var headers = {
-    Authorization: "Bearer " + token,
-    "Content-Type": "application/json",
-  };
+    // Construct the headers with the token
+    var headers = {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    };
 
-  // Make the Axios POST request with the token in the headers
-  axios
-    .post("https://strapi-deployment-xh5t.onrender.com/api/links", postData, {
-      headers: headers,
-    })
-    .then(function (response) {
-      console.log("POST request successful:", response.data);
-      // Handle success, if needed
-    })
-    .catch(function (error) {
-      console.error("Error making POST request:", error);
-      // Handle error, if needed
-    });
+    // Make the Axios POST request with the token in the headers
+    axios
+      .post("https://strapi-deployment-xh5t.onrender.com/api/links", postData, {
+        headers: headers,
+      })
+      .then(function (response) {
+        console.log("POST request successful:", response.data);
+        // Handle success, if needed
+      })
+      .catch(function (error) {
+        console.error("Error making POST request:", error);
+        // Handle error, if needed
+      });
+  });
 });
